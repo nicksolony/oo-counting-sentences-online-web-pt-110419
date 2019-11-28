357e8955b0f74f16dbc59e206bba4d9c322c4159
Nick Solonyy <nicksolony@gmail.com>
2019-11-28 02:26:04 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -9,15 +9,11 @@ class String
   end
 
   def question?
-      self.end_with?(".")
+      self.end_with?("!")
   end
 
   def exclamation?
-    if self.end_with?("!")
-      true
-    else
-      false
-    end
+      self.end_with?("!")
   end
 
   def count_sentences
